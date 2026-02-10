b4c4c1de5a5aa1b2b8b0894f998d531ee9402dbf
Ainhoa <ainhoaleonarrieta@gmail.com>
2026-02-10 20:53:41 +0100

    add global ValidationPipe and error handling in bootstrap

--- expense-tracker-backend/src/main.ts
+++ expense-tracker-backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Activar validaciones globales usando los DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Elimina campos no definidos en DTO
+      forbidNonWhitelisted: true, // Lanza error si llega un campo extra
+      transform: true, // Convierte automáticamente tipos
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
