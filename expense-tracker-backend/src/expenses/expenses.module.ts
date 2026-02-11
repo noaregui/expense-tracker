be16f6e801bdd4895e8612006a709081873b7090
Ainhoa <ainhoaleonarrieta@gmail.com>
2026-02-11 13:55:56 +0100

    register create, list and update use cases as providers in ExpensesModule

--- expense-tracker-backend/src/expenses/expenses.module.ts
+++ expense-tracker-backend/src/expenses/expenses.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ExpensesController } from './expenses.controller';
+import { CreateExpenseUseCase } from 'src/application/expenses/create-expense.use-case';
+import { ListExpensesUseCase } from 'src/application/expenses/list-expenses.use-case';
+import { UpdateExpenseUseCase } from 'src/application/expenses/update-expense.use-case';
 import { ExpensesService } from './expenses.service';
-
 @Module({
   controllers: [ExpensesController],
-  providers: [ExpensesService],
+  providers: [
+    ExpensesService,
+    CreateExpenseUseCase,
+    ListExpensesUseCase,
+    UpdateExpenseUseCase,
+  ],
 })
 export class ExpensesModule {}
