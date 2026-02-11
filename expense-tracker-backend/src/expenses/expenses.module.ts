fa92ff60934ebff4daf8116760a673a8b3154f49
Ainhoa <ainhoaleonarrieta@gmail.com>
2026-02-11 14:48:42 +0100

    add import ExpensesRepository

--- expense-tracker-backend/src/expenses/expenses.module.ts
+++ expense-tracker-backend/src/expenses/expenses.module.ts
@@ -3,11 +3,11 @@ import { ExpensesController } from './expenses.controller';
 import { CreateExpenseUseCase } from 'src/application/expenses/create-expense.use-case';
 import { ListExpensesUseCase } from 'src/application/expenses/list-expenses.use-case';
 import { UpdateExpenseUseCase } from 'src/application/expenses/update-expense.use-case';
-import { ExpensesService } from './expenses.service';
+import { ExpensesRepository } from 'src/infraestructure/expenses.repository';
 @Module({
   controllers: [ExpensesController],
   providers: [
-    ExpensesService,
+    ExpensesRepository,
     CreateExpenseUseCase,
     ListExpensesUseCase,
     UpdateExpenseUseCase,
