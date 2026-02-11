69da6aa8bf3ee2b2390de8e2a91ef5c302a68e41
Ainhoa <ainhoaleonarrieta@gmail.com>
2026-02-11 13:55:19 +0100

    update ExpensesController to use application use cases instead of service directly

--- expense-tracker-backend/src/expenses/expenses.controller.ts
+++ expense-tracker-backend/src/expenses/expenses.controller.ts
@@ -1,27 +1,34 @@
 import { Controller, Get, Post, Patch, Body, Param } from '@nestjs/common';
-import { ExpensesService } from './expenses.service';
+import { CreateExpenseUseCase } from '../application/expenses/create-expense.use-case';
+import { ListExpensesUseCase } from '../application/expenses/list-expenses.use-case';
+import { UpdateExpenseUseCase } from '../application/expenses/update-expense.use-case';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
+import { Expense } from 'src/domain/entities/expense.entity';
 
 @Controller('expenses')
 export class ExpensesController {
-  constructor(private readonly expensesService: ExpensesService) {}
+  constructor(
+    private readonly createExpenseUseCase: CreateExpenseUseCase,
+    private readonly listExpensesUseCase: ListExpensesUseCase,
+    private readonly updateExpenseUseCase: UpdateExpenseUseCase,
+  ) {}
 
   @Get()
-  findAll() {
-    return this.expensesService.findAll();
+  findAll(): Expense[] {
+    return this.listExpensesUseCase.execute();
   }
 
   @Post()
-  create(@Body() createExpenseDto: CreateExpenseDto) {
-    return this.expensesService.create(createExpenseDto);
+  create(@Body() createExpenseDto: CreateExpenseDto): Expense {
+    return this.createExpenseUseCase.execute(createExpenseDto);
   }
 
   @Patch(':id')
   updatePartial(
     @Param('id') id: string,
     @Body() updateExpenseDto: UpdateExpenseDto,
-  ) {
-    return this.expensesService.update(id, updateExpenseDto);
+  ): Expense {
+    return this.updateExpenseUseCase.execute(id, updateExpenseDto);
   }
 }
